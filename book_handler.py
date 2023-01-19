c3a73b2cb4e0dce2b37e7d48a5572448437a929b
julian <julian0104226@gmail.com>
2023-01-19 18:20:05 -0500

    validacion formatos desde json y retorno de informacion en formato json

--- book_handler.py
+++ book_handler.py
@@ -1,31 +1,51 @@
 from http.server import BaseHTTPRequestHandler 
 from data_opener import DataOpener
-from urllib.parse import  parse_qs, parse_qsl
+from urllib.parse import  parse_qsl
+
+import json
 
 
 class BookHandler(BaseHTTPRequestHandler): 
     
-    def do_GET(self): 
-        data = DataOpener().open_data()
+    def do_GET(self):
+        data_opener = DataOpener()
+        books = data_opener.open_data("books.json")
+        formats = data_opener.open_data("formats.json")
         params = self.__get_params()
+        body = {}
         
-        if 'book' in params.keys():
+        if 'book' in params.keys() and self.__check_format(formats, params['format']):
+            body['format'] = params['format']
+            
             self.send_response(200) 
-            self.send_header('Content-Type', params['format']) 
+            self.send_header('Content-Type', "text") 
             self.end_headers() 
-            self.__open_page(data, params)
+            body = self.__build_body(books, params, body)
+            self.wfile.write(str(json.dumps(body)).encode())
         else: 
             self.send_response(404) 
-            
-    def __open_page(self, data : dict, params: dict):
-        for book in data:
-                if book['name'] == params['book']:
+    
+    def __check_format(self, formats : list, selected_format : str):
+        for format in formats:
+            if selected_format == format['format']:
+                return True
+        return False
+
+    def __build_body(self, books : dict, params: dict, body : dict):
+        for book in books:
+                if book['id'] == int(params['book']) and self.__check_page(book['pages'], int(params['page'])):
+                    body['book'] = book['name']
+                    
                     for page in book['pages']:
                         if str(page['page']) == params['page']:
-                            self.wfile.write(page["content"].encode())
+                            body['page'] = page["content"]
+                            return body
                 else:
                     self.send_response(404)
     
-    def __get_params(self):
+    def __check_page(self, pages : list, page : int) -> bool:
+        return page <= len(pages)
+    
+    def __get_params(self) -> dict:
         return dict(parse_qsl(self.path[1:]))
     
--- data_opener.py
+++ data_opener.py
@@ -3,7 +3,8 @@ import json
 
 class DataOpener:
     
-    def open_data(self):
-        with open("prueba.json") as data_file:
+    def open_data(self, file_name : str) -> dict:
+        with open(file_name) as data_file:
             data = json.load(data_file)
+        data_file.close()
         return data
